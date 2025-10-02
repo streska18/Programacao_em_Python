519d08cff887d5125831ea1f81eb5dd23524e1fa
Suele Cristine Treska <su_treska_18@hotmail.com>
2025-10-01 21:39:24 -0300

    Aulas de 01 - 06 do curso de Programação em Python

---
+++ Suele/aula2/# %% [markdown].py
@@ -0,0 +1,81 @@
+# %% [markdown]
+# # Python Fundamentos - Aula 2
+
+# %% [markdown]
+# ## Números
+# ---
+
+# %%
+import platform #visualizar a versão do Python que estou utilizando
+
+print ("Versão da linguagem Python usada neste Jupyter Notebook", platform.python_version())
+
+# %% [markdown]
+# ### Números e Operações Matemáticas
+
+# %% [markdown]
+# Pressione as teclas CTRL + ALT + ENTER para executar o código em uma célula ou Pressione o botão Play, no canto superior direito
+
+# %%
+# Soma de dois números
+
+4 + 4
+
+# %%
+# Subtração de dois números
+
+4 - 3
+
+# %%
+# Multiplicação de dois números
+
+3 * 3
+
+# %%
+# Divisão de dois números:
+
+3 / 2
+
+# %%
+# Divisão inteira
+
+3 // 2
+
+# %%
+# Módulo
+
+10 % 3
+
+# %% [markdown]
+# # Função Type
+# ---
+# 
+# 
+
+# %%
+type(12)
+
+# %%
+a = " Eu sou uma 'string' "
+print(a)
+type(a)
+
+# %%
+a = ' Eu sou uma "string" '
+print(a)
+type(a)
+
+# %% [markdown]
+# # Operações com números Float
+# 
+# 
+
+# %%
+print(3.1 + 6.4)
+
+4 + 4.0
+
+# %% [markdown]
+# #
+
+
